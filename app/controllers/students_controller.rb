f35e448d7f391d365e813bab8003f0d8112d7476
Jharell Chambers <jharell.chambers@selu.edu>
2020-04-08 05:27:39 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,10 +9,11 @@ class StudentsController < ApplicationController
   end
   
   def new 
+        Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+  redirect_to new_student_path
   end 
   
   def create
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-  redirect_to new_student_path
+
  end 
 end
